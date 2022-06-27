f782d3830be7be8ce07c74c606881daa4378f2bd
Taylor Li <258266864@qq.com>
2022-06-27 11:18:59 +0800

    to 17丨解析图片和字体

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,38 @@
 "use strict";
 
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js',
-    },
-    mode: 'production',
-}
+  entry: {
+    index: "./src/index.js",
+    search: "./src/search.js",
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.less$/i,
+        use: [
+          // compiles Less to CSS
+          "style-loader",
+          "css-loader",
+          "less-loader",
+        ],
+      },
+    ],
+  },
+  //   plugins: [new HtmlWebpackPlugin()],
+};

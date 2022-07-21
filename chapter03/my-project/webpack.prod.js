541e49862f6dca5c3d7acc85d7f7dc9c05b72819
Taylor Li <258266864@qq.com>
2022-07-21 23:22:33 +0800

    47丨初级分析：使用webpack内置的stats

--- chapter03/my-project/webpack.prod.js
+++ chapter03/my-project/webpack.prod.js
@@ -15,39 +15,38 @@ const setMPA = () => {
     const htmlWebpackPlugins = [];
     const entryFiles = glob.sync(path.join(__dirname, './src/*/index.js'));
 
-    Object.keys(entryFiles)
-        .map((index) => {
-            const entryFile = entryFiles[index];
-            // '/Users/cpselvis/my-project/src/index/index.js'
+    Object.keys(entryFiles).map((index) => {
+        const entryFile = entryFiles[index];
+        // '/Users/cpselvis/my-project/src/index/index.js'
 
-            const match = entryFile.match(/src\/(.*)\/index\.js/);
-            const pageName = match && match[1];
+        const match = entryFile.match(/src\/(.*)\/index\.js/);
+        const pageName = match && match[1];
 
-            entry[pageName] = entryFile;
-            htmlWebpackPlugins.push(
-                new HtmlWebpackPlugin({
-                    inlineSource: '.css$',
-                    template: path.join(__dirname, `src/${pageName}/index.html`),
-                    filename: `${pageName}.html`,
-                    chunks: ['vendors', pageName],
-                    inject: true,
-                    minify: {
-                        html5: true,
-                        collapseWhitespace: true,
-                        preserveLineBreaks: false,
-                        minifyCSS: true,
-                        minifyJS: true,
-                        removeComments: false
-                    }
-                })
-            );
-        });
+        entry[pageName] = entryFile;
+        htmlWebpackPlugins.push(
+            new HtmlWebpackPlugin({
+                inlineSource: '.css$',
+                template: path.join(__dirname, `src/${pageName}/index.html`),
+                filename: `${pageName}.html`,
+                chunks: ['vendors', pageName],
+                inject: true,
+                minify: {
+                    html5: true,
+                    collapseWhitespace: true,
+                    preserveLineBreaks: false,
+                    minifyCSS: true,
+                    minifyJS: true,
+                    removeComments: false,
+                },
+            })
+        );
+    });
 
     return {
         entry,
-        htmlWebpackPlugins
-    }
-}
+        htmlWebpackPlugins,
+    };
+};
 
 const { entry, htmlWebpackPlugins } = setMPA();
 
@@ -55,7 +54,7 @@ module.exports = {
     entry: entry,
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: '[name]_[chunkhash:8].js'
+        filename: '[name]_[chunkhash:8].js',
     },
     mode: 'production',
     module: {
@@ -65,14 +64,11 @@ module.exports = {
                 use: [
                     'babel-loader',
                     // 'eslint-loader'
-                ]
+                ],
             },
             {
                 test: /.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader'
-                ]
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
             },
             {
                 test: /.less$/,
@@ -85,19 +81,19 @@ module.exports = {
                         options: {
                             plugins: () => [
                                 require('autoprefixer')({
-                                    browsers: ['last 2 version', '>1%', 'ios 7']
-                                })
-                            ]
-                        }
+                                    browsers: ['last 2 version', '>1%', 'ios 7'],
+                                }),
+                            ],
+                        },
                     },
                     {
                         loader: 'px2rem-loader',
                         options: {
                             remUnit: 75,
-                            remPrecision: 8
-                        }
-                    }
-                ]
+                            remPrecision: 8,
+                        },
+                    },
+                ],
             },
             {
                 test: /.(png|jpg|gif|jpeg)$/,
@@ -105,10 +101,10 @@ module.exports = {
                     {
                         loader: 'file-loader',
                         options: {
-                            name: '[name]_[hash:8].[ext]'
-                        }
-                    }
-                ]
+                            name: '[name]_[hash:8].[ext]',
+                        },
+                    },
+                ],
             },
             {
                 test: /.(woff|woff2|eot|ttf|otf)$/,
@@ -116,46 +112,49 @@ module.exports = {
                     {
                         loader: 'file-loader',
                         options: {
-                            name: '[name]_[hash:8][ext]'
-                        }
-                    }
-                ]
-            }
-        ]
+                            name: '[name]_[hash:8][ext]',
+                        },
+                    },
+                ],
+            },
+        ],
     },
     plugins: [
         new MiniCssExtractPlugin({
-            filename: '[name]_[contenthash:8].css'
+            filename: '[name]_[contenthash:8].css',
         }),
         new OptimizeCSSAssetsPlugin({
             assetNameRegExp: /\.css$/g,
-            cssProcessor: require('cssnano')
+            cssProcessor: require('cssnano'),
         }),
         new CleanWebpackPlugin(),
         new HtmlWebpackExternalsPlugin({
             externals: [
-              {
-                module: 'react',
-                entry: 'https://11.url.cn/now/lib/16.2.0/react.min.js',
-                global: 'React',
-              },
-              {
-                module: 'react-dom',
-                entry: 'https://11.url.cn/now/lib/16.2.0/react-dom.min.js',
-                global: 'ReactDOM',
-              },
-            ]
+                {
+                    module: 'react',
+                    entry: 'https://11.url.cn/now/lib/16.2.0/react.min.js',
+                    global: 'React',
+                },
+                {
+                    module: 'react-dom',
+                    entry: 'https://11.url.cn/now/lib/16.2.0/react-dom.min.js',
+                    global: 'ReactDOM',
+                },
+            ],
         }),
         new FriendlyErrorsWebpackPlugin(),
-        function() {
+        function () {
             this.hooks.done.tap('done', (stats) => {
-                if (stats.compilation.errors && stats.compilation.errors.length && process.argv.indexOf('--watch') == -1)
-                {
+                if (
+                    stats.compilation.errors &&
+          stats.compilation.errors.length &&
+          process.argv.indexOf('--watch') == -1
+                ) {
                     console.log('build error');
                     process.exit(1);
                 }
-            })
-        }    
+            });
+        },
     ].concat(htmlWebpackPlugins),
     // optimization: {
     //     splitChunks: {
@@ -169,5 +168,5 @@ module.exports = {
     //         }
     //     }
     // }
-    stats: 'errors-only'
+    stats: 'errors-only',
 };
